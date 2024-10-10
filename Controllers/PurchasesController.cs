d0d28877cb7d55cd517ac716a928dcb264f63058
VeronikaTereshchenko <verteresh@gmail.com>
2024-10-10 20:31:33 +0300

    Added: RateLimiter, cache. Modified: logging

---
+++ Controllers/PurchasesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parser._ASP.Net.Interfaces;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace Parser._ASP.Net.Controllers
+{
+    [ApiController]
+    [EnableRateLimiting("purchaseLimiter")]
+    public class PurchasesController : ControllerBase
+    {
+        private IWebParser _parser;
+
+        public PurchasesController(IWebParser parser) 
+        {
+            _parser = parser;
+        }
+
+        [Route("api/[controller]/[action]")]
+        [EnableRateLimiting("concurrencyLimiter")]
+        [HttpGet]
+        public async Task<IActionResult> GetPurchases() 
+        {
+            var parsedPurchasesList = await _parser.GetPageInfoAsync();
+
+            return Ok(parsedPurchasesList);
+        }
+    }
+}
